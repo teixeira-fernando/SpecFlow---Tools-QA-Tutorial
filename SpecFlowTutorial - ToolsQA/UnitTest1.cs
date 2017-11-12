605b52359a5f6660a067da81ba22551ed0367f7a
teixeira-fernando <fernandoadt1@gmail.com>
2017-11-12 08:02:59 -0200

    Adicionar arquivos de projeto.

---
+++ SpecFlowTutorial - ToolsQA/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecFlowTutorial___ToolsQA
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
